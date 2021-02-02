5adfaf364478ce6c0120099f01d42954117eae72
Todd Walls <t_e_walls@yahoo.com>
2021-02-01 23:01:01 -0700

    this is my last push before submitting for a grade.

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function (){
+
 // establish local date and time variables
 let localDate = "";
 let localTime = "";
@@ -7,6 +8,8 @@ let scheduleHour = 0;
 localDate = dayjs().format('dddd, MMMM D');
 console.log(localDate);
 localTime = dayjs().hour();
+// localTime = 9;
+
 // localTime = dayjs().subtract(12, 'hour');
 console.log(localTime);
 
@@ -15,37 +18,53 @@ $("#currentDay").html(localDate);
 
 // loop through time blocks and compare different time blocks to currrent time
 function setBlockColor () {
-    
-    $(".hour-block").each(function(){
-        // Test if the div element is empty
-    //     if($(this).is(":empty")){
-    //         $(this).css("background", "yellow");
-    //     }
-    // for (var i = $('data-idx'); i < 9; i++) {
-        // console.log(i);
-        scheduleHour = parseInt($('.this').data("id"));
-        // let scheduleHour = parseInt(scheduleText);
-        console.log(scheduleHour);
-    // if (localTime > scheduleHour) {
-    //     $('.hour-block').addClass('past');
-    // } else if (localTime < scheduleHour) {
-    //     $('.hour-block').addClass('future');
-    // } else {
-    //     $('.hour-block').addClass('present');
-    // }
-
-    if (localTime == scheduleHour) {
+    $(".hour-block").each(function(index, element){
+        scheduleHour = parseInt($('.hour-block').data("id"));
+        // console.log(scheduleHour);
+// if time block is same time as current time add to time block class to present
+    if (localTime === scheduleHour) {
         $('.hour-block').addClass('present');
+// if time block is later that current time add to time block class to future
     } else if (localTime < scheduleHour) {
         $('.hour-block').addClass('future');
+// if time block is earlier than current time add to time block class to past
     } else {
         $('.hour-block').addClass('past');
     }
+    console.log(scheduleHour);
+    return false;
 })
-// if time block is earlier than current time change time block class to past
-// if time block is same time as current time change time block class to present
-// if time block is later that current time change time block class to future
+}
+//save text area input to local storage after 'lock' button is clicked
+function saveEvent(){
+    $('textarea').each(function(){    
+        var id = $(this).attr('id');
+        var value = $(this).val();
+       localStorage.setItem(id, value);
+
+    });   
+};
+// content entered into the textarea will remain after the page is reloaded
+function contentPersist(){
+    $('textarea').each(function(){    
+        var id = $(this).attr('id');
+        var value = localStorage.getItem(id);
+
+        $(this).val(value);
+
+    }); 
+
+
+
+
+
 }
 
+
+
+
 setBlockColor();
+contentPersist();
+$('.saveBtn').on("click", saveEvent);
+
 }); 
--- README.md
+++ README.md
@@ -2,9 +2,18 @@
 
 #
 I decided to use the Luxon third party API for date and time. 
+# 
+Lunox documanetaiton was very dfficult to follow, so I went with dayjs API instead.
 
 ##
 Since the HTML and CSS have been created, I need to see how they link together to create the necessary divs to organize the scheduler. The hour blocks go from 9AM to 5PM. Each text field has a button to add the event to that hour block and saves it to local storage. One the divs are created, I can create a JS file to make the scheduler interactive.
 
 ###
-When the scheduler is opened, using Luxon, the local time and date needs to be requested and returned. The current date will be displayed at the top of the page. The current hour block needs a background of green and I assume no new content can be added to the field. The hour blocks before the current hour block need to be set to grey and not able to add content to them. The hour blocks after the current hour clock need to have color set to red and new content added needs to be saved to local storage and content not disappear . The stored content also needs to be linked to the hour block it was added (perhaps that will be inevitable).
+When the scheduler is opened, using dayjs, the local time and date need to be requested and returned. The current date will be displayed at the top of the page. The current hour block needs a background of green. The hour blocks before the current hour block need to be set to grey. The hour blocks after the current hour clock need to have color set to red. New content added needs to be saved to local storage and content not disappear in the time block when the page is refreshed. 
+
+#### 
+I have been able to add the background color to all the time blocks. Unfortunately, at this point, I am adding the same color to all of the time blocks and not relative to the current time. The .each loop keeps repeating the result of the first time block.
+
+#####
+screenshot of scheduler:
+(/Work-Day-Scheduler/Assets/screenshots/Work Day Scheduler.png?raw=true "")
